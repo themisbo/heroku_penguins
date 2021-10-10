13f720d1c0a5d9f3e05480e4951fb4498c409d7e
Themistoklis Botsas <themisbo@windowslive.com>
2021-10-10 21:04:08 +0100

    added files

---
+++ app/app.py
@@ -0,0 +1,25 @@
+from main import classify
+from flask import Flask, request, render_template
+
+app = Flask(__name__, template_folder="templates")
+
+@app.route('/')
+def home():
+    return render_template('home.html') 
+
+@app.route('/classify',methods=['POST','GET'])
+def classify_type():
+    try:
+        get_bill_length_mm = request.args.get('blmm')
+        get_bill_depth_mm = request.args.get('bdmm')
+        get_flipper_length_mm = request.args.get('flmm')
+        get_body_mass_g = request.args.get('bmg')
+
+        variety = classify(get_bill_length_mm, get_bill_depth_mm, get_flipper_length_mm, get_body_mass_g)
+        
+        return render_template('output.html', variety=variety)
+    except:
+        return 'Error'
+
+if(__name__=='__main__'):
+    app.run()        
---
+++ app/main.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn import svm
+from sklearn.impute import KNNImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+
+penguins = pd.read_csv('PalmerPenguins.csv')
+penguins = penguins.dropna()
+penguins = penguins.drop("year", axis = 1)
+penguins = penguins.drop("island", axis = 1)
+penguins = penguins.drop("sex", axis = 1)
+
+variety_mappings = {0: 'Adelie', 1: 'Gentoo', 2: 'Chinstrap'}
+
+penguins = penguins.replace(['Adelie', 'Gentoo' , 'Chinstrap'],[0, 1, 2])
+
+species = penguins.pop('species')
+
+pipe_svc = Pipeline([ ('onehot', OneHotEncoder(handle_unknown='ignore', sparse=False)), ('imputer', KNNImputer(n_neighbors=2, weights="uniform")), ('scaler', StandardScaler(with_mean=False)), ('svc', svm.SVC())])
+
+pipe_svc.fit(penguins, species)
+
+def classify(bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g):
+    arr = np.array([bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g])
+    arr = arr.astype(np.float64)
+    query = arr.reshape(1, -1)
+
+    prediction = variety_mappings[pipe_svc.predict(query)[0]]
+
+    return prediction
+
+# island ="Biscoe"
+# bill_length_mm = 48
+# bill_depth_mm = 16
+# flipper_length_mm = 220
+# body_mass_g = 5400
+# sex = "male"
